ecce14b1a341ec7689d9fd36fe5b40aa887f3be4
matheuscesarr <matheuscesarr@gmail.com>
2023-07-16 18:50:52 -0300

    first commit

---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,52 @@
+﻿int number = 0;
+bool confirm = false;
+do {
+Console.WriteLine("Enter an integer value between 5 and 10");
+string answer = Console.ReadLine();
+confirm = Int32.TryParse(answer,out number);
+if (confirm){
+        if (number >=5 && number<=10){
+        Console.WriteLine($"Your input value ({number}) has been accepted.");
+        } else {
+             Console.WriteLine($"You entered {number}. Please enter a number between 5 and 10.");
+        }
+}
+else {
+    Console.WriteLine("Sorry, you entered an invalid number, please try again");
+}
+} while (!confirm);
+
+string role= "";
+bool confirm2 = false;
+do {
+        Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+    role = Console.ReadLine().Trim().ToLower();
+    confirm2=true;
+        switch (role)
+        {
+
+            case "administrator":
+            Console.WriteLine("Your input value (Administrator) has been accepted.");
+            break;
+
+            case "manager":
+            Console.WriteLine("Your input value (Manager) has been accepted.");
+            break;
+
+            case "user":
+            Console.WriteLine("Your input value (User) has been accepted.");
+            break;
+
+            default:
+            Console.WriteLine($@"The role name that you entered, ""{role}"" is not valid.");
+            confirm2=false;
+            break;
+
+        } 
+   
+
+} while (!confirm2);
+
+
+Console.WriteLine($@"You are the {role[0].ToString().ToUpper() + role.Substring(1)} number {number}!");
+    
