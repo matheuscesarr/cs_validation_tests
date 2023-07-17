96769357a4a0acffea70a0508c373e2eee03c82b
Matheus Cesar Rodrigues <108311862+matheuscesarr@users.noreply.github.com>
2023-07-16 21:17:38 -0300

    deleting unnecessary files

--- past/example_for.cs
+++
@@ -1,9 +0,0 @@
-﻿
-for (int i=1;i<101;i++){
-        string message = "";
-        if (i%3==0 && i%5==0){message = $"{i} - FizzBuzz";}
-        else if (i%3==0) {message = $"{i} - Fizz";}
-        else if (i%5==0) {message = $"{i} - Buzz";}
-        else {message = $"{i}";}
-        Console.WriteLine(message);
-}
